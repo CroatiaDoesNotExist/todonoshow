58e23c20fd7ec96303cb0ab8ab9f5f9671da46ff
CroatiaDoesNotExist <38951620+CroatiaDoesNotExist@users.noreply.github.com>
2018-05-03 15:58:42 +0200

    Add files via upload

---
+++ src/containers/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Todo from './Todo'
+
+const mapStateToProps = state => ({
+  todos: state.todos
+})
+
+const TodoList = ({todos}) => (
+  <ul>
+  {
+    todos.map(todo =>
+      <Todo
+      key = {todo.id}
+      {...todo}
+      />
+    )
+  }
+  </ul>
+)
+
+export default connect(
+  mapStateToProps
+)(TodoList)
